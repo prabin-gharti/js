9bfba9b5af322a4cdefb7c4d37e86558197a08aa
gt-ayush <gt-ayush@outlook.com>
2025-11-04 15:52:33 +0530

    js start

---
+++ app.js
@@ -0,0 +1,42 @@
+let m=80
+
+/* 
+hello
+i am good byyy
+
+*/
+
+if (m <=95 & m>90){
+    console.log("A+")
+
+
+}
+
+else if (m <=90 & m>80){
+    console.log("A")
+
+
+}
+else if (m <=80 & m>70){
+    console.log("B")
+
+
+}
+
+else if (m <=70 & m>60){
+    console.log("c")
+
+
+}
+
+else if (m <=50 & m>60){
+    console.log("d")
+
+
+}
+
+else{
+    console.log("f")
+
+
+}
